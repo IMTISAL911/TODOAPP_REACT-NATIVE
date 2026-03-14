0d75e28c19f50ab45f2a60317382261f50bb5a38
IMTISAL HUSSAIN <imtisalhussan@gmail.com>
2026-03-14 20:15:15 +0500

    Initial commit of todo App

---
+++ App.jsx
@@ -0,0 +1,75 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
+
+// import { NewAppScreen } from '@react-native/new-app-screen';
+// import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import {
+//   SafeAreaProvider,
+//   useSafeAreaInsets,
+// } from 'react-native-safe-area-context';
+
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   return (
+//     <SafeAreaProvider>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <AppContent />
+//     </SafeAreaProvider>
+//   );
+// }
+
+// function AppContent() {
+//   const safeAreaInsets = useSafeAreaInsets();
+
+//   return (
+//     <View style={styles.container}>
+//       <NewAppScreen
+//         templateFileName="App.tsx"
+//         safeAreaInsets={safeAreaInsets}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
+// export default App;
+
+
+
+
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>HELLO WORLD</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black", // added background
+  },
+  heading: {
+    color: "white",
+    fontSize: 24, // optional for better visibility
+    fontWeight: "bold",
+  },
+});
+
+export default App;
