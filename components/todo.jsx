5e729dcab43b6caaf1f0a3ceba77737f176754b9
IMTISAL HUSSAIN <imtisalhussan@gmail.com>
2026-03-18 21:02:25 +0500

    todo complete

--- components/todo.jsx
+++ components/todo.jsx
@@ -1,17 +1,23 @@
 import React,{useState} from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 
 
 const Todo = () =>{
     const [text, setTxt] = useState("");
     const [display, setDisplay]= useState([])
+    // const [display, setDisplay]= useState("")
 
     const add =() =>{
     // console.warn("hello buddy");
     if(text.trim() === "") return;
     setDisplay([...display,text])
     setTxt("");
+    }
+
+    const dleet = () =>{
+        setDisplay([""]);
+        setTxt("");
     }
     return(
         <View style={styles.container}>
@@ -28,12 +34,40 @@ const Todo = () =>{
                 </TouchableOpacity>
             </View>
 
-            <View style={{width:"90%", backgroundColor:"#222" ,padding: 20, borderRadius: 10,}}>
-   {display.map((item,index)=>(
+            {/* <View style={{width:"90%", backgroundColor:"#222" ,padding: 20, borderRadius: 10,}}> */}
+   {/* {display.map((item,index)=>(
     <Text key={index} style={{color:"white"}}>{item}</Text>
-   ))}
+   ))} */}
+
+   {/* <Text>Typing...</Text>
+   <Text>{text || "waiting for input..."}</Text>
+
+   <View>
+    <Text>last added:</Text>
+    <Text>{display || "nothing added yet"}</Text>
+   </View>
                 
-            </View>
+            </View> */}
+<ScrollView
+style={{flex:1}}
+contentContainerStyle={{flexGrow: 1,  paddingHorizontal:20}}
+showsVerticalScrollIndicator={false}
+>
+<View style={[styles.resultView,{width:"100%"}]}>
+       <Text style={styles.label}>Typing..</Text>
+         <Text style={styles.liveText}>{text || "Waiting for Input..."}</Text>
+                 <View style={{ marginTop: 20 }}>
+           <Text style={styles.label}>Last Added :</Text>
+           {display.map((item,index)=>(
+            <Text key={index} style={styles.liveText}>{item || "nothing added yet"}</Text>
+           ))}
+           {/* <Text style={styles.liveText}>{display || "nothing added yet"}</Text> */}
+         </View>
+       </View>
+</ScrollView>
+<TouchableOpacity style={styles.delete} onPress={dleet}>
+    <Text style={styles.dlettext}>DLEET</Text>
+</TouchableOpacity>
         </View>
     )
 }
@@ -72,7 +106,41 @@ const styles = StyleSheet.create({
     textbtn:{
         color:"white",
         fontWeight:"bold"
-    }
+    },
+    resultView: {
+    width: "90%",
+    padding: 20,
+    backgroundColor: "#222",
+    borderRadius: 10,
+  },
+  label: {
+    color: "#888",
+    fontSize: 12,
+    // textTransf,
+  },
+
+  liveText: {
+    color: "yellow",
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 5,
+  },
+  delete:{
+    backgroundColor:"red",
+    height:50,
+    paddingHorizontal:20,
+    // alignItems:"center",
+    justifyContent:'center',
+    marginBottom:10,
+    marginTop:10,
+    borderRadius:10,
+    
+  },
+  dlettext:{
+   
+color:"white",
+fontWeight:"bold"
+  },
 })
 
 export default Todo
